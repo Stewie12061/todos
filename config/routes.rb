dc6a94e2a16fa82e7a6e87639774050ec9779ac8
Stewie12061 <nhut170501@gmail.com>
2021-05-24 14:43:41 +0700

    add bootstrap fontawesome devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get '/contact', controller: :contact, action: :index
+  get '/about', controller: :about, action: :index
+  get '/tasks', controller: :tasks, action: :index
+  devise_for :users, controllers: {
+        sessions: 'users/sessions',
+        registrations: 'users/registrations',
+        passwords: 'users/passwords'
+      }
   root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
